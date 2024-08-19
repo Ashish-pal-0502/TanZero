192a8d6c77b21b11168f8b2f37a56479dbe64785
Ashish Pal <ashishpal0502@gmail.com>
2024-08-19 21:55:25 +0530

    connect

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -16,8 +16,8 @@ const Contact = () => {
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            for any query and info about product feel free to call anytime we
-            24X7 available
+            For any query and info about product feel free to call anytime we
+            are 24X7 available.
           </p>
           <p className="mt-3">
             <BiMailSend /> : tanzerotech@gmail.com
